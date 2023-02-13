797053402542926972207634d0bb27115bbe5efe
Tim McLennan <102727510+Tim-Mclennan@users.noreply.github.com>
2023-02-13 14:28:52 +1100

    added formatting and indentation fixes, as well as updating the put method

--- employee-list/src/components/AddEmployee/AddEmployee.tsx
+++ employee-list/src/components/AddEmployee/AddEmployee.tsx
@@ -63,161 +63,57 @@ const AddEmployee = () => {
         <form  className={styles.AddEmployee__Form} onSubmit={handleFormSubmit}>
             <h4>Personal Information</h4>
             <label htmlFor="firstName">First Name:</label>
-            <input
-            type="text"
-            name="firstName"
-            id="firstName"
-            placeholder="First Name"
-            value={formData.firstName}
-            onChange={handleInputChange}
-            required
-            />
+            <input type="text" name="firstName" id="firstName" placeholder="First Name" value={formData.firstName} onChange={handleInputChange} required/>
 
             <label htmlFor="middleName">Middle Name: (if applicable)</label>
-            <input
-            type="text"
-            name="middleName"
-            id="middleName"
-            placeholder="Middle Name"
-            value={formData.middleName}
-            onChange={handleInputChange}
-            />
+            <input type="text" name="middleName" id="middleName" placeholder="Middle Name" value={formData.middleName} onChange={handleInputChange}/>
 
             <label htmlFor="lastName">Last Name:</label>
-            <input
-            type="text"
-            name="lastName"
-            id="lastName"
-            placeholder="Last Name"
-            value={formData.lastName}
-            onChange={handleInputChange}
-            required
-            />
+            <input type="text" name="lastName" id="lastName" placeholder="Last Name" value={formData.lastName} onChange={handleInputChange} required/>
             <br></br>
             <h4>Contact Details</h4>
             <label htmlFor="email">Email:</label>
-            <input
-            type="email"
-            name="email"
-            id="email"
-            placeholder="Email"
-            value={formData.email}
-            onChange={handleInputChange}
-            required
-            />
+            <input type="email" name="email" id="email" placeholder="Email" value={formData.email} onChange={handleInputChange} required/>
 
             <label htmlFor="mobile">Mobile Number:</label>
             <p><span id={styles.numberNote} >Must be an Australian number with 10 digits</span></p>
-            <input
-            type="tel"
-            name="mobile"
-            id="mobile"
-            placeholder="Mobile"
-            value={formData.mobile}
-            onChange={handleInputChange}
-            required
-            max="10"
-            min="10"
-            />
+            <input type="tel" name="mobile" id="mobile" placeholder="Mobile" value={formData.mobile} onChange={handleInputChange} required max="10" min="10"/>
 
             <label htmlFor="residentialAddress">Residential Address:</label>
-            <input
-            type="text"
-            name="residentialAddress"
-            id="residentialAddress"
-            placeholder="Residential Address"
-            value={formData.residentialAddress}
-            onChange={handleInputChange}
-            />
+            <input type="text" name="residentialAddress" id="residentialAddress" placeholder="Residential Address" value={formData.residentialAddress} onChange={handleInputChange}/>
             <br></br>
             <h4>Employee Status</h4>
             <p>What is your contract type?</p>
             <div className={styles.AddEmployee__Form__Contract}>
                 <div>
-                    <input
-                    type="radio"
-                    id="permanent"
-                    name="contractType"
-                    value="Permanent"
-                    checked={formData.contractType === "Permanent"}
-                    onChange={handleInputChange}
-                    required
-                    />
+                    <input type="radio" id="permanent" name="contractType" value="Permanent" checked={formData.contractType === "Permanent"} onChange={handleInputChange} required/>
                     <label htmlFor="permanent">Permanent</label>
                 </div>
                 <div>
-                    <input
-                    type="radio"
-                    id="contract"
-                    name="contractType"
-                    value="Contract"
-                    checked={formData.contractType === "Contract"}
-                    onChange={handleInputChange}
-                    required
-                    />
+                    <input type="radio" id="contract" name="contractType" value="Contract" checked={formData.contractType === "Contract"} onChange={handleInputChange} required/>
                     <label htmlFor="contract">Contract</label>
                 </div>
             </div>
             <br></br>
             <label htmlFor="startDate">Start Date:</label>
-            <input
-            type="date"
-            name="startDate"
-            id="startDate"
-            placeholder="Start Date"
-            value={formData.startDate}
-            onChange={handleInputChange}
-            required
-            />
+            <input type="date" name="startDate" id="startDate" placeholder="Start Date" value={formData.startDate} onChange={handleInputChange} required/>
 
             <label htmlFor="endDate">End Date:</label>
-            <input
-            type="date"
-            name="endDate"
-            id="endDate"
-            placeholder="End Date"
-            value={formData.endDate}
-            onChange={handleInputChange}
-            required
-            />
+            <input type="date" name="endDate" id="endDate" placeholder="End Date" value={formData.endDate} onChange={handleInputChange} required/>
             <p>Is this on a part-time or full-time basis?</p>
             <div className={styles.AddEmployee__Form__timeBasis}>
                 <div>
-                    <input
-                    type="radio"
-                    id="part-time"
-                    name="timeBasis"
-                    value="Part-time"
-                    checked={formData.timeBasis === "Part-time"}
-                    onChange={handleInputChange}
-                    required
-                    />
+                    <input type="radio" id="part-time" name="timeBasis" value="Part-time" checked={formData.timeBasis === "Part-time"} onChange={handleInputChange} required/>
                     <label htmlFor="part-time">Part-time</label>
                 </div>
                 <div>
-                    <input
-                    type="radio"
-                    id="full-time"
-                    name="timeBasis"
-                    value="Full-time"
-                    checked={formData.timeBasis === "Full-time"}
-                    onChange={handleInputChange}
-                    required
-                    />
+                    <input type="radio" id="full-time" name="timeBasis" value="Full-time" checked={formData.timeBasis === "Full-time"} onChange={handleInputChange} required/>
                     <label htmlFor="full-time">Full-time</label>
                 </div>
             </div>
             <br></br>
             <label htmlFor="hoursPerWeek">Hours Per Week:</label>
-            <input
-            type="number"
-            name="hoursPerWeek"
-            id="hoursPerWeek"
-            placeholder="Hours Per Week"
-            value={formData.hoursPerWeek}
-            onChange={handleInputChange}
-            required
-            />
+            <input type="number" name="hoursPerWeek" id="hoursPerWeek" placeholder="Hours Per Week" value={formData.hoursPerWeek} onChange={handleInputChange} required/>
             <div>
                 <button className={styles.AddEmployee__Form__SaveBtn} type="submit">Save</button>
                 <Link to="/">
--- employee-list/src/components/EmployeeDetails/EmployeeDetails.tsx
+++ employee-list/src/components/EmployeeDetails/EmployeeDetails.tsx
@@ -52,21 +52,7 @@ const EmployeeDetails = () => {
         timeBasis,
         hoursPerWeek
     } = formData;
-    axios
-        .put(`/employees/${id}`, {
-        id,
-        firstName,
-        middleName,
-        lastName,
-        email,
-        mobile,
-        residentialAddress,
-        contractType,
-        startDate,
-        endDate,
-        timeBasis,
-        hoursPerWeek
-        })
+    axios.put(`/employees/${id}`, formData)
         .then(res => {
         console.log(res);
         })
@@ -90,152 +76,58 @@ const EmployeeDetails = () => {
         <form  className={styles.EmployeeDetails__Form} onSubmit={handleSubmit}>
             <h4>Personal Information</h4>
             <label htmlFor="firstName">First Name:</label>
-            <input
-            type="text"
-            name="firstName"
-            id="firstName"
-            placeholder="First Name"
-            value={formData.firstName}
-            onChange={handleInputChange}
-            />
+            <input type="text" name="firstName" id="firstName" placeholder="First Name" value={formData.firstName} onChange={handleInputChange}/>
 
             <label htmlFor="middleName">Middle Name: (if applicable)</label>
-            <input
-            type="text"
-            name="middleName"
-            id="middleName"
-            placeholder="Middle Name"
-            value={formData.middleName}
-            onChange={handleInputChange}
-            />
+            <input type="text" name="middleName" id="middleName" placeholder="Middle Name" value={formData.middleName} onChange={handleInputChange} />
 
             <label htmlFor="lastName">Last Name:</label>
-            <input
-            type="text"
-            name="lastName"
-            id="lastName"
-            placeholder="Last Name"
-            value={formData.lastName}
-            onChange={handleInputChange}
-            />
+            <input type="text" name="lastName" id="lastName" placeholder="Last Name" value={formData.lastName} onChange={handleInputChange} />
             <br></br>
             <h4>Contact Details</h4>
             <label htmlFor="email">Email:</label>
-            <input
-            type="email"
-            name="email"
-            id="email"
-            placeholder="Email"
-            value={formData.email}
-            onChange={handleInputChange}
-            />
+            <input type="email" name="email" id="email" placeholder="Email" value={formData.email} onChange={handleInputChange}/>
 
             <label htmlFor="mobile">Mobile Number:</label>
             <p><span id={styles.numberNote} >Must be an Australian number</span></p>
-            <input
-            type="tel"
-            name="mobile"
-            id="mobile"
-            placeholder="Mobile"
-            value={formData.mobile}
-            onChange={handleInputChange}
-            />
+            <input type="tel" name="mobile" id="mobile" placeholder="Mobile" value={formData.mobile} onChange={handleInputChange} />
 
             <label htmlFor="residentialAddress">Residential Address:</label>
-            <input
-            type="text"
-            name="residentialAddress"
-            id="residentialAddress"
-            placeholder="Residential Address"
-            value={formData.residentialAddress}
-            onChange={handleInputChange}
-            />
+            <input type="text" name="residentialAddress" id="residentialAddress" placeholder="Residential Address" value={formData.residentialAddress} onChange={handleInputChange}/>
             <br></br>
             <h4>Employee Status</h4>
             <p>What is your contract type?</p>
             <div className={styles.EmployeeDetails__Form__Contract}>
                 <div>
-                    <input
-                    type="radio"
-                    id="permanent"
-                    name="contractType"
-                    value="Permanent"
-                    checked={formData.contractType === "Permanent"}
-                    onChange={handleInputChange}
-                    required
-                    />
+                    <input type="radio" id="permanent" name="contractType" value="Permanent" checked={formData.contractType === "Permanent"} onChange={handleInputChange}  required/>
                     <label htmlFor="permanent">Permanent</label>
                 </div>
                 <div>
-                    <input
-                    type="radio"
-                    id="contract"
-                    name="contractType"
-                    value="Contract"
-                    checked={formData.contractType === "Contract"}
-                    onChange={handleInputChange}
-                    required
-                    />
+                    <input type="radio" id="contract" name="contractType" value="Contract" checked={formData.contractType === "Contract"}
+                    onChange={handleInputChange} required />
                     <label htmlFor="contract">Contract</label>
                 </div>
             </div>
             <br></br>
             <label htmlFor="startDate">Start Date:</label>
-            <input
-            type="date"
-            name="startDate"
-            id="startDate"
-            placeholder="Start Date"
-            value={formData.startDate}
-            onChange={handleInputChange}
-            />
-
+            <input type="date" name="startDate" id="startDate" placeholder="Start Date" value={formData.startDate} onChange={handleInputChange}/>
+            
             <label htmlFor="endDate">End Date:</label>
-            <input
-            type="date"
-            name="endDate"
-            id="endDate"
-            placeholder="End Date"
-            value={formData.endDate}
-            onChange={handleInputChange}
-            />
+            <input type="date" name="endDate" id="endDate" placeholder="End Date" value={formData.endDate} onChange={handleInputChange}/>
             <p>Is this on a part-time or full-time basis?</p>
             <div className={styles.EmployeeDetails__Form__timeBasis}>
                 <div>
-                    <input
-                    type="radio"
-                    id="part-time"
-                    name="timeBasis"
-                    value="Part-time"
-                    checked={formData.timeBasis === "Part-time"}
-                    onChange={handleInputChange}
-                    required
-                    />
+                    <input type="radio" id="part-time" name="timeBasis" value="Part-time" checked={formData.timeBasis === "Part-time"}    onChange={handleInputChange} required/>
                     <label htmlFor="part-time">Part-time</label>
                 </div>
                 <div>
-                    <input
-                    type="radio"
-                    id="full-time"
-                    name="timeBasis"
-                    value="Full-time"
-                    checked={formData.timeBasis === "Full-time"}
-                    onChange={handleInputChange}
-                    required
-                    />
+                    <input type="radio" id="full-time" name="timeBasis"value="Full-time" checked={formData.timeBasis === "Full-time"} onChange={handleInputChange} required/>
                     <label htmlFor="full-time">Full-time</label>
                 </div>
             </div>
             <br></br>
             <label htmlFor="hoursPerWeek">Hours Per Week:</label>
-            <input
-            type="number"
-            name="hoursPerWeek"
-            id="hoursPerWeek"
-            placeholder="Hours Per Week"
-            value={formData.hoursPerWeek}
-            onChange={handleInputChange}
-            />
+            <input type="number" name="hoursPerWeek" id="hoursPerWeek" placeholder="Hours Per Week" value={formData.hoursPerWeek} onChange={handleInputChange}/>
             <div>
                 <button className={styles.EmployeeDetails__Form__SaveBtn} type="submit">Save</button>
                 <Link to="/">

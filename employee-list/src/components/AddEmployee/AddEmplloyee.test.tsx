ea3cf6f004570eed90eaf07537c8076aa200f54a
Tim McLennan <102727510+Tim-Mclennan@users.noreply.github.com>
2023-02-14 11:22:44 +1100

    formatted tests

--- employee-list/src/components/AddEmployee/AddEmplloyee.test.tsx
+++ employee-list/src/components/AddEmployee/AddEmplloyee.test.tsx
@@ -4,122 +4,120 @@ import AddEmployee from "./AddEmployee";
 
 jest.mock("axios");
 
-export const renderForm = (mockData: any) => (
-    <AddEmployee />
-  );
-  
-  describe("Add Employee Component", () => {
-    let renderResult: any;
-  
-    beforeEach(() => {
-      renderResult = render(renderForm({}));
-    });
-  
-    it("renders the correct heading", () => {
-      const { getByText } = renderResult;
-      const heading = getByText("New Employee Details");
-      expect(heading).toBeInTheDocument();
-    });
-  
-    it("renders the correct subheading", () => {
-      const { getByText } = renderResult;
-      const subheading = getByText("Personal Information");
-      expect(subheading).toBeInTheDocument();
-    });
-  
-    it("renders the first name input field", () => {
-      const { getByLabelText } = renderResult;
-      const firstNameInput = getByLabelText("First Name:");
-      expect(firstNameInput).toBeInTheDocument();
-    });
-  
-    it("renders the middle name input field", () => {
-      const { getByLabelText } = renderResult;
-      const middleNameInput = getByLabelText("Middle Name: (if applicable)");
-      expect(middleNameInput).toBeInTheDocument();
-    });
-  
-    it("renders the last name input field", () => {
-      const { getByLabelText } = renderResult;
-      const lastNameInput = getByLabelText("Last Name:");
-      expect(lastNameInput).toBeInTheDocument();
-    });
-  
-    it("renders the email input field", () => {
-      const { getByLabelText } = renderResult;
-      const emailInput = getByLabelText("Email:");
-      expect(emailInput).toBeInTheDocument();
-    });
-  
-    it("renders the mobile number input field", () => {
-      const { getByLabelText } = renderResult;
-      const mobileInput = getByLabelText("Mobile Number:");
-      expect(mobileInput).toBeInTheDocument();
-    });
-  
-    it("renders the residential address input field", () => {
-      const { getByLabelText } = renderResult;
-      const residentialAddressInput = getByLabelText("Residential Address:");
-      expect(residentialAddressInput).toBeInTheDocument();
-    });
-  
-    it("renders the contract type input field", () => {
-      const { getByLabelText } = renderResult;
-      const contractTypeInput = getByLabelText("Contract Type:");
-      expect(contractTypeInput).toBeInTheDocument();
-    });
-  
-    it("renders the start date input field", () => {
-      const { getByLabelText } = renderResult;
-      const startDateInput = getByLabelText("Start Date:");
-      expect(startDateInput).toBeInTheDocument();
-    });
-  
-    it("renders the end date input field", () => {
-      const { getByLabelText } = renderResult;
-      const endDateInput = getByLabelText("End Date:");
-      expect(endDateInput).toBeInTheDocument();
-    });
-  
-    it("renders the time basis input field", () => {
-      const { getByLabelText } = renderResult;
-      const timeBasisInput = getByLabelText("Time Basis:");
-      expect(timeBasisInput).toBeInTheDocument();
-      });
-      
-      it("renders the submit button", () => {
-      const { getByText } = renderResult;
-      const submitButton = getByText("Submit");
-      expect(submitButton).toBeInTheDocument();
-      });
-      
-      it("submits the form data via axios", async () => {
-        // arrange
-        const formData = {
-          firstName: "John",
-          lastName: "Doe",
-          email: "john.doe@example.com",
-          mobile: "1234567890",
-        };
-        const postSpy = jest.spyOn(axios, "post");
-        postSpy.mockResolvedValue({});
-    
-        // act
-        const { getByLabelText, getByText } = render(<AddEmployee />);
-        const firstNameInput = getByLabelText("First Name:");
-        const lastNameInput = getByLabelText("Last Name:");
-        const emailInput = getByLabelText("Email:");
-        const mobileInput = getByLabelText("Mobile Number:");
-        const submitButton = getByText("Submit");
-    
-        fireEvent.change(firstNameInput, { target: { value: formData.firstName } });
-        fireEvent.change(lastNameInput, { target: { value: formData.lastName } });
-        fireEvent.change(emailInput, { target: { value: formData.email } });
-        fireEvent.change(mobileInput, { target: { value: formData.mobile } });
-        fireEvent.click(submitButton);
-    
-        // assert
-        await new Promise(resolve => setTimeout(resolve));
-        expect(postSpy).toHaveBeenCalledWith("/employees", formData);
-      });
+export const renderForm = (mockData: any) => <AddEmployee />;
+
+describe("Add Employee Component", () => {
+  let renderResult: any;
+
+  beforeEach(() => {
+    renderResult = render(renderForm({}));
+  });
+
+  it("renders the correct heading", () => {
+    const { getByText } = renderResult;
+    const heading = getByText("New Employee Details");
+    expect(heading).toBeInTheDocument();
+  });
+
+  it("renders the correct subheading", () => {
+    const { getByText } = renderResult;
+    const subheading = getByText("Personal Information");
+    expect(subheading).toBeInTheDocument();
+  });
+
+  it("renders the first name input field", () => {
+    const { getByLabelText } = renderResult;
+    const firstNameInput = getByLabelText("First Name:");
+    expect(firstNameInput).toBeInTheDocument();
+  });
+
+  it("renders the middle name input field", () => {
+    const { getByLabelText } = renderResult;
+    const middleNameInput = getByLabelText("Middle Name: (if applicable)");
+    expect(middleNameInput).toBeInTheDocument();
+  });
+
+  it("renders the last name input field", () => {
+    const { getByLabelText } = renderResult;
+    const lastNameInput = getByLabelText("Last Name:");
+    expect(lastNameInput).toBeInTheDocument();
+  });
+
+  it("renders the email input field", () => {
+    const { getByLabelText } = renderResult;
+    const emailInput = getByLabelText("Email:");
+    expect(emailInput).toBeInTheDocument();
+  });
+
+  it("renders the mobile number input field", () => {
+    const { getByLabelText } = renderResult;
+    const mobileInput = getByLabelText("Mobile Number:");
+    expect(mobileInput).toBeInTheDocument();
+  });
+
+  it("renders the residential address input field", () => {
+    const { getByLabelText } = renderResult;
+    const residentialAddressInput = getByLabelText("Residential Address:");
+    expect(residentialAddressInput).toBeInTheDocument();
+  });
+
+  it("renders the contract type input field", () => {
+    const { getByLabelText } = renderResult;
+    const contractTypeInput = getByLabelText("Contract Type:");
+    expect(contractTypeInput).toBeInTheDocument();
+  });
+
+  it("renders the start date input field", () => {
+    const { getByLabelText } = renderResult;
+    const startDateInput = getByLabelText("Start Date:");
+    expect(startDateInput).toBeInTheDocument();
+  });
+
+  it("renders the end date input field", () => {
+    const { getByLabelText } = renderResult;
+    const endDateInput = getByLabelText("End Date:");
+    expect(endDateInput).toBeInTheDocument();
+  });
+
+  it("renders the time basis input field", () => {
+    const { getByLabelText } = renderResult;
+    const timeBasisInput = getByLabelText("Time Basis:");
+    expect(timeBasisInput).toBeInTheDocument();
+  });
+
+  it("renders the submit button", () => {
+    const { getByText } = renderResult;
+    const submitButton = getByText("Submit");
+    expect(submitButton).toBeInTheDocument();
+  });
+
+  it("submits the form data via axios", async () => {
+    // arrange
+    const formData = {
+      firstName: "John",
+      lastName: "Doe",
+      email: "john.doe@example.com",
+      mobile: "1234567890",
+    };
+    const postSpy = jest.spyOn(axios, "post");
+    postSpy.mockResolvedValue({});
+
+    // act
+    const { getByLabelText, getByText } = render(<AddEmployee />);
+    const firstNameInput = getByLabelText("First Name:");
+    const lastNameInput = getByLabelText("Last Name:");
+    const emailInput = getByLabelText("Email:");
+    const mobileInput = getByLabelText("Mobile Number:");
+    const submitButton = getByText("Submit");
+
+    fireEvent.change(firstNameInput, { target: { value: formData.firstName } });
+    fireEvent.change(lastNameInput, { target: { value: formData.lastName } });
+    fireEvent.change(emailInput, { target: { value: formData.email } });
+    fireEvent.change(mobileInput, { target: { value: formData.mobile } });
+    fireEvent.click(submitButton);
+
+    // assert
+    await new Promise((resolve) => setTimeout(resolve));
+    expect(postSpy).toHaveBeenCalledWith("/employees", formData);
+  });
 });
--- employee-list/src/components/EmployeeCard/EmployeeCard.test.tsx
+++ employee-list/src/components/EmployeeCard/EmployeeCard.test.tsx
@@ -1,31 +1,31 @@
-import axios from 'axios';
-import axiosMock from 'axios-mock-adapter';
-import EmployeeCard from './EmployeeCard';
-import { render, fireEvent } from '@testing-library/react';
+import axios from "axios";
+import axiosMock from "axios-mock-adapter";
+import EmployeeCard from "./EmployeeCard";
+import { render, fireEvent } from "@testing-library/react";
 
 const mock = new axiosMock(axios);
 
-describe('EmployeeCard component', () => {
-  it('deletes an employee', async () => {
+describe("EmployeeCard component", () => {
+  it("deletes an employee", async () => {
     const employee = {
       id: 1,
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'johndoe@example.com',
-      mobile: '1234567890',
-      residentialAddress: '123 Main St',
-      contractType: 'Full Time',
-      startDate: '01/01/2022',
-      endDate: '01/01/2023',
-      timeBasis: '40 hours',
-      hoursPerWeek: 40
+      firstName: "John",
+      lastName: "Doe",
+      email: "johndoe@example.com",
+      mobile: "1234567890",
+      residentialAddress: "123 Main St",
+      contractType: "Full Time",
+      startDate: "01/01/2022",
+      endDate: "01/01/2023",
+      timeBasis: "40 hours",
+      hoursPerWeek: 40,
     };
 
     mock.onDelete(`/employees/${employee.id}`).reply(200);
 
     const { getByText } = render(<EmployeeCard employee={employee} />);
 
-    fireEvent.click(getByText('Remove'));
+    fireEvent.click(getByText("Remove"));
 
     expect(mock.history.delete.length).toBe(1);
     expect(mock.history.delete[0].url).toBe(`/employees/${employee.id}`);
